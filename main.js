2bbf09c0b58ca7ccf3b87e772db13a46fdae622a
Robert Reichel <robertreich1212@gmail.com>
2021-04-16 20:27:48 +0200

    revert some stuff

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 window.onscroll = function () { scrollFunction() }
 
 function scrollFunction () {
-	const scroller = document.querySelector('.scroll-to-top')
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		scroller.classList.add('show')
-	} else {
-		scroller.classList.remove('show')
-	}
+  const scroller = document.querySelector('.scroll-to-top')
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scroller.classList.add('show')
+  } else {
+    scroller.classList.remove('show')
+  }
 }
